3f4ffb040633a89c91642221f7ad0bd98c29e915
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-10-16 15:51:13 +0700

    Connect to mongoDB && create API for fetch customers detail and add customer

--- 3P_Back/model/User.js
+++ 3P_Back/model/User.js
@@ -1,25 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    // username:{
-    //     type: String,
-    //     required: true
-    // },
-    // roles:{
-    //     User:{
-    //         type: Number,
-    //         default: 2000
-    //     },
-    //     Editor: Number,
-    //     Admin: Number
-    // },
-    // password:{
-    //     type: String,
-    //     required: true
-    // },
-    // refreshToken: String
-});
+const userSchema = new mongoose.Schema({}, { collection: 'customers' });
 
 
 module.exports = mongoose.model('User', userSchema)
